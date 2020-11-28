e50b1b6ca019674c6f8e421c43c1d871ac7b1066
Taylor Preston <taylor.preston.music@gmail.com>
2020-11-28 16:46:44 -0600

    add env var

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+
+const puppy = process.env.PUPPY || "Fido"
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    console.log(`${puppy} is the best puppy!`);
     await sleep(5000);
   }
 }
